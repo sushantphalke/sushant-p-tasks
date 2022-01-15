7862e4a01badc3bb59a83b01e9e0ac52c8d9543e
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-15 17:07:59 +0530

    task 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Counters } from './components/sections/counters';
+
+function App() {
+  return (
+    <div className="App">
+     <Counters/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sections/counters.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export const Counters = () => {
+    return (
+        <>
+            <div className='counter-section'>
+                <div className='counter-up'>
+                    <div className='content'>
+                        <div className ='box'>
+                            <div className='icon'>
+                                <i className ='fas fa-history'></i>
+                            </div>
+                            <div className='counter'>5000</div>
+                            <div className='text'>STUDENTS</div>
+                        </div>
+                        <div className='box'>
+                            <div className='icon'>
+                                <i className ='fas fa-gift'></i>
+                            </div>
+                            <div className='counter'>500</div>
+                            <div className='text'>FACULTY</div>
+                        </div>
+                        <div className='box'>
+                            <div className='icon'>
+                                <i className ='fas fa-users'></i>
+                            </div>
+                            <div className='counter'>400</div>
+                            <div className='text'>STAFF</div>
+                        </div>
+                        <div className='box'>
+                            <div className='icon'>
+                                <i className ='fas fa-award'></i>
+                            </div>
+                            <div className='counter'>100</div>
+                            <div className='text'>Awards Received</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
