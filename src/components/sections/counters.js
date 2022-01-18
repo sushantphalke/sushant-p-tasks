c1c492824a19ecbe1f52b05e4d245413f1692a5d
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-01-18 16:41:21 +0530

    exam forms and sbi collect links added

--- src/components/sections/counters.js
+++ src/components/sections/counters.js
@@ -3,35 +3,35 @@ import React from 'react';
 export const Counters = () => {
     return (
         <><div class='container'>
-            <div className='counter-section' style={{backgroundColor:'cyan', border:'1px solid red',borderRadius:'10px'}}>
+            <div className='counter-section' style={{backgroundColor:'#ffc9bf', border:'1px solid red',borderRadius:'10px'}}>
                 <div className='counter-up'>
                     <div className='content'>
                         <div className ='box'>
                             <div className='icon'>
                                 <i className ='fas fa-history'></i>
                             </div>
-                            <div className='counter'>5000</div>
+                            <div className='counter' style={{color:'#9c8c89'}}>5000</div>
                             <div className='text'>STUDENTS</div>
                         </div>
                         <div className='box'>
                             <div className='icon'>
-                                <i className ='fas fa-gift'></i>
+                                <i className ='fas fa-users' ></i>
                             </div>
-                            <div className='counter'>500</div>
+                            <div className='counter'style={{color:'#9c8c89'}}>500</div>
                             <div className='text'>FACULTY</div>
                         </div>
                         <div className='box'>
                             <div className='icon'>
                                 <i className ='fas fa-users'></i>
                             </div>
-                            <div className='counter'>400</div>
+                            <div className='counter'style={{color:'#9c8c89'}}>400</div>
                             <div className='text'>STAFF</div>
                         </div>
                         <div className='box'>
                             <div className='icon'>
                                 <i className ='fas fa-award'></i>
                             </div>
-                            <div className='counter'>100</div>
+                            <div className='counter'style={{color:'#9c8c89'}}>100</div>
                             <div className='text'>Awards Received</div>
                         </div>
                     </div>
--- src/components/sections/implinks.js
+++ src/components/sections/implinks.js
@@ -3,29 +3,39 @@ import React from 'react';
 export default function Implinks() {
     return (
         <div className='container'>
-            <ol class="list-group list-group-numbered">
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold"><a href='https://mahadbtmahait.gov.in'>MahaDBT</a></div>
-      https://mahadbtmahait.gov.in/
-    </div>
-    <span class="badge bg-primary rounded-pill">4</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold"><a href='http://sim.unipune.ac.in/sim_app/Login/Login?ReturnUrl=%2fStudent_Main%2fDashBoard_Student.aspx'>Exam Forms</a></div>
-      examforms unipune
-    </div>
-    <span class="badge bg-primary rounded-pill">3</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start">
-    <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Content for list item
-    </div>
-    <span class="badge bg-primary rounded-pill">14</span>
-  </li>
-</ol>
+            <ol class='list-group list-group-numbered' >
+                <li class='list-group-item d-flex justify-content-between align-items-start' style={{backgroundColor:'#ffc9bf', border:'1px solid red',borderRadius:'10px'}}>
+                    <div class='ms-2 me-auto'>
+                        <div class='fw-bold'>
+                            <a href='https://mahadbtmahait.gov.in'>MahaDBT</a>
+                        </div>
+                        https://mahadbtmahait.gov.in
+                    </div>
+                    <span class='badge bg-primary rounded-pill'>4</span>
+                </li>
+                <li class='list-group-item d-flex justify-content-between align-items-start'style={{backgroundColor:'#ffc9bf', border:'1px solid red',borderRadius:'10px'}}>
+                    <div class='ms-2 me-auto'>
+                        <div class='fw-bold'>
+                            <a href='https://examform.unipune.ac.in/'>
+                                Exam Forms
+                            </a>
+                        </div>
+                        https://examform.unipune.ac.in
+                    </div>
+                    <span class='badge bg-primary rounded-pill'>3</span>
+                </li>
+                <li class='list-group-item d-flex justify-content-between align-items-start'style={{backgroundColor:'#ffc9bf', border:'1px solid red',borderRadius:'10px'}}>
+                    <div class='ms-2 me-auto'>
+                        <div class='fw-bold'>
+                            <a href='https://www.onlinesbi.com/sbicollect/icollecthome.htm'>
+                               SBI Collect
+                            </a>
+                        </div>
+                        https://www.onlinesbi.com/sbicollect/icollecthome.htm
+                    </div>
+                    <span class='badge bg-primary rounded-pill'>3</span>
+                </li>
+            </ol>
         </div>
     );
 }
